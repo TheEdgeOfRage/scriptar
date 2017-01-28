a9a717849278f5802ca7dab0672c543bdc7ee61e
Pavle Portic <pavle.portic@gmail.com>
2017-01-28 15:25:06 +0100

    Initial setup

---
+++ scriptar.py
@@ -0,0 +1,21 @@
+#! venv/bin/python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 pavle <pavle@hp-arch>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+#! venv/bin/python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 pavle <pavle@hp-arch>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+from scriptar import app as application
+
+if __name__ == "__main__":
+    application.run()
