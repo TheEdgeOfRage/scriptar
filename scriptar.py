c3e7c1a2881b02b81955db6f6d4a457a52079e40
Pavle Portic <pavle.portic@gmail.com>
2017-01-28 19:32:39 +0100

    [app] add login route

--- scriptar.py
+++ scriptar.py
@@ -11,14 +11,18 @@
 """
 
 import os
-from flask import Flask
+from flask import Flask, request, session, render_template
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-@app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+@app.route('/')
+def index():
+    return '<h1 style="color:blue">Hello There!</h1>'
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run()
