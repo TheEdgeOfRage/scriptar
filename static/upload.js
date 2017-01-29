d1ecd2dfe570e087f115267d1ce5ddea58e4f0d7
Milos Miljanic <milosmilos96@gmail.com>
2017-01-29 04:10:18 +0100

    [upload]

--- static/upload.js
+++ static/upload.js
@@ -9,18 +9,14 @@ $('#upload-input').on('change', function(){
     var files = $(this).get(0).files;
 
     if (files.length > 0){
-        // create a FormData object which will be sent as the data payload in the
-        // AJAX request
         var formData = new FormData();
 
-        // loop through all the selected files and add them to the formData object
-        // for (var i = 0; i < files.length; i++) {
-        //     var file = files[i];
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
 
-        //     // add the files to formData object for the data payload
-        //     formData.append('uploads[]', file, file.name);
-        // }
-        formData.append('file', files[0], files[0].name);
+            formData.append('uploads[]', file, file.name);
+        }
+        // formData.append('file', files[0], files[0].name);
     
         $.ajax({
             url: '/upload',
