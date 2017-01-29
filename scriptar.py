e08a1a598d94fc1f99ff956d87a4ab33e940752d
Milos Miljanic <milosmilos96@gmail.com>
2017-01-29 03:57:26 +0100

    [upload]

--- scriptar.py
+++ scriptar.py
@@ -47,9 +47,31 @@ def file_upload():
         else:
             return render_template('file_upload.html')
     elif request.method == 'POST':
-        f = request.files['file']
-        f.save('/srv/http/scriptar/uploads/uploaded_file.txt')
-        return redirect(url_for('index'))
+        # uploaded_files = request.files['file']
+        # for file in uploaded_files:
+        #     file.save('/srv/http/scriptar/uploads/uploaded_file.txt')
+        # return redirect(url_for('index'))
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            # flash('No file part')
+            return redirect(url_for('file_upload'))
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            # flash('No selected file')
+            return redirect(url_for('file_upload'))
+        if file: #and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join('/srv/http/scriptar/uploads/', filename))
+            # return redirect(url_for('uploaded_file', filename=filename))
+            return 'kurac'
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
