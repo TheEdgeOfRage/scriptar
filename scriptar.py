148adf05928f6299b791f0bc5d9ae72193d56e10
Pavle Portic <pavle.portic@gmail.com>
2017-01-30 04:15:57 +0100

    [signup] fix debugging

--- scriptar.py
+++ scriptar.py
@@ -64,9 +64,9 @@ def signup():
             result = item
             break
 
-        app.logger.debug(result)
         if result:
             close_db(db, cur)
+            app.logger.debug(result.fetchall())
             flash(result.fetchall()[0], 'error')
             return render_template('signup.html', username=username, email=email, name=name)
 
